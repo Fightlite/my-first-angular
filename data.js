373a605eaeb7cac76f3c185cae0a48b2dee2d50e
Nhan Tran <88392476+Fightlite@users.noreply.github.com>
2022-01-21 14:11:05 +0700

    Create data.js to store data

---
+++ data.js
@@ -0,0 +1,39 @@
+const people = {
+  "students": [
+    {
+      "code": "MSV-01",
+      "gender": "male",
+      "firstName": "Tran",
+      "lastName": "Nhan",
+      "dob": "1999/1/1",
+      "phone": "123-456-7899",
+      "email": "codeotran@gmail.com",
+      "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP0GPv0ztd4FQK7sUufzkr-YdrfE3VeBN3FibeEgH2t8AmW5PWaxV2z8bIRIvgB6UtT14&usqp=CAU",
+      "id": 1
+    },
+    {
+      "code": "MSV-02",
+      "gender": "female",
+      "firstName": "Nhan",
+      "lastName": "Tran",
+      "dob": "1999/1/1",
+      "phone": "123-456-7899",
+      "email": "codelagnotran@gmail.com",
+      "picture": "https://cafebiz.cafebizcdn.vn/thumb_w/600/162123310254002176/2021/8/30/photo1630305337256-1630305337442104252003.jpg",
+      "id": 2
+    },
+    {
+      "code": "MSV-03",
+      "gender": "male",
+      "firstName": "An",
+      "lastName": "Nguyen",
+      "dob": "01/01/1999",
+      "phone": "1234567889",
+      "email": "mail@gmail.com",
+      "picture": "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_01_08/messi-3245.jpeg",
+      "id": 3
+    }
+  ]
+}
+
+module.exports = { people }
